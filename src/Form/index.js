4e4e06dcc789f43bc01e6bc8c711ac8e6d6f85b3
Wioletta Koperska <koperskawioletta@gmail.com>
2025-02-10 19:30:19 +0100

    changes and import Form

---
+++ src/Form/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Form = () => (
+    <form className="form">
+        <input className="form__input" placeholder="Dodaj nowe zadanie.."/>
+        <button className="form__button">Dodaj zadanie</button>
+    </form>
+)
+
+export default Form;

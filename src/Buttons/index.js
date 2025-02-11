6c9697853913fea493d5e59dd1462e1b50b095ea
Wioletta Koperska <koperskawioletta@gmail.com>
2025-02-11 20:03:18 +0100

    changes for buttons

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,23 +1,26 @@
 import React from "react";
 import "./style.css"
 
-const Buttons = ({tasks, hideDoneTasks}) => (
-        <div className="buttons">
-            {tasks.length > 0 && (
-                <React.Fragment>
-                    <button className="buttons__button">
-                        {hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone
-                    </button>
-                    <button 
-                        className="buttons__button"
-                        disabled={tasks.every(({done}) => done)}            
-                    >
-                        Ukończ wszystkie
-                    </button>
-                </React.Fragment>
-            )}
-        </div>
-    
+const Buttons = ({tasks, hideDoneTasks, toggleHideDoneTasks, markAllAsDone}) => (
+    <div className="buttons-container">
+        {tasks.length > 0 && (
+            <React.Fragment>
+                <button
+                    className="buttons__button"
+                    onClick={toggleHideDoneTasks}
+                >
+                    {hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone
+                </button>
+                <button 
+                    className="buttons__button"
+                    onClick={markAllAsDone}
+                    disabled={tasks.every(({done}) => done)}            
+                >
+                    Ukończ wszystkie
+                </button>
+            </React.Fragment>
+        )}
+    </div>
 );
 
 export default Buttons;

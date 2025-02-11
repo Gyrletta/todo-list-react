ef2fad8ca3af6a47823fa2f3ac6bf484540c60a9
Wioletta Koperska <koperskawioletta@gmail.com>
2025-02-11 01:18:41 +0100

    feat: Add React.Fragment to wrap buttons in Buttons component

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -2,19 +2,25 @@ import React from "react";
 import "./style.css"
 
 const Buttons = ({tasks, hideDoneTasks}) => (
-     tasks.length > 0 && (
         <div className="buttons">
-            <button className="buttons__button">
-            {hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone
-        </button>
-        <button 
-            className="buttons__button"
-            disabled={tasks.every(({done}) => done)}            
-        >
-            Ukończ wszystkie
-        </button>
+            {tasks.length > 0 && (
+                <React.Fragment>
+                    <button className="buttons__button">
+                        {hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone
+                    </button>
+                    <button 
+                        className="buttons__button"
+                        disabled={tasks.every(({done}) => done)}            
+                    >
+                        Ukończ wszystkie
+                    </button>
+                </React.Fragment>
+            )};
         </div>
-    )
+    
 );
 
 export default Buttons;
+
+
+

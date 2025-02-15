e1a951f99b86cf2ce371da76350ec8112e33743f
Wioletta Koperska <koperskawioletta@gmail.com>
2025-02-15 13:42:24 +0100

    Refactor: Move tasks data to a separate file and import it into App component

--- src/App.js
+++ src/App.js
@@ -5,33 +5,22 @@ import Buttons from "./Buttons";
 import Section from "./Section";
 import Header from "./Header";
 import Container from "./Container";
-
-const tasks = [
-  {id: 1, content: "przejść na Reacta", done: false},
-  {id: 2, content: "zjeść obiad", done: true},
-];
+import tasks from "./Tasks/tasksData";
 
 const hideDoneTasks = false;
 
 function App() {
   return (
     <Container>
-        <Header title="Lista zadań" />
-        <Section 
-          title="Dodaj nowe zadanie" 
-          body={
-            <Form />
-          } 
-        />
-        <Section
+      <Header title="Lista zadań" />
+      <Section title="Dodaj nowe zadanie" body={<Form />} />
+      <Section
         title="Lista zadań"
-        body={
-          <Tasks tasks={tasks} hideDoneTasks={hideDoneTasks}/>
-        }
+        body={<Tasks tasks={tasks} hideDoneTasks={hideDoneTasks} />}
         extraHeaderContent={
           <Buttons tasks={tasks} hideDoneTasks={hideDoneTasks} />
         }
-        />
+      />
     </Container>
   );
 }
---
+++ src/Tasks/tasksData.js
@@ -0,0 +1,6 @@
+const tasks = [
+  { id: 1, content: "przejść na Reacta", done: false },
+  { id: 2, content: "zjeść obiad", done: true },
+];
+
+export default tasks;

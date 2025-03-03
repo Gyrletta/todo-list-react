d1a2b1f4cdc68e52ad00b5dce6dc69027bb316a0
Wioletta Koperska <koperskawioletta@gmail.com>
2025-03-03 23:08:00 +0100

    create theme

---
+++ src/theme.js
@@ -0,0 +1,17 @@
+export const theme = {
+  colors: {
+    teal: "#008080",
+    silver: "#cccccc",
+    dark: "#333",
+    white: "#fff",
+    alto: "#ddd",
+    green: "#28a745",
+    primary: "#f0f0f0",
+    light: "#999",
+    lightGrey: "#f9f9f9",
+    red: "#ff0000",
+  },
+  breakpoints: {
+    mobileMax: "767",
+  },
+};

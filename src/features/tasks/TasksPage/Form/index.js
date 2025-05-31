4940209479d45e253efbf62b3c1d32a51ba30986
Wioletta Koperska <koperskawioletta@gmail.com>
2025-05-31 15:56:46 +0100

    feat(tasks): add search input with query param sync and styles

--- src/features/tasks/TasksPage/Form/index.js
+++ src/features/tasks/TasksPage/Form/index.js
@@ -1,8 +1,9 @@
 import React, { useState, useRef } from "react";
 import { useDispatch } from "react-redux";
 import { nanoid } from "@reduxjs/toolkit";
-import { Button, Input, StyledForm } from "./styled";
+import { Button, StyledForm } from "./styled";
 import { addTask } from "../../tasksSlice";
+import { Input } from "../Input";
 
 const Form = () => {
   const [newTaskContent, setNewTaskContent] = useState("");
